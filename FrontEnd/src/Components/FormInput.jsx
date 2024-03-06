a25a525a1b18c90724ea2e74387398e7cd0382be
Elvin Posadas <elvinjosuep2@gmail.com>
2024-03-06 08:37:10 -0600

    Added skeletons for the login and signup forms

---
+++ FrontEnd/src/Components/FormInput.jsx
@@ -0,0 +1,12 @@
+import styles from "./formInput.module.css";
+
+const FormInput = () => {
+  return (
+    <div className="formInput">
+      <label>Username</label>
+      <input></input>
+    </div>
+  );
+};
+
+export default FormInput;
---
+++ FrontEnd/src/Pages/Signup.jsx
@@ -0,0 +1,14 @@
+import FormInput from "../Components/FormInput";
+
+const Signup = () => {
+  return (
+    <div className="login-container">
+      <FormInput></FormInput>
+      <FormInput></FormInput>
+      <FormInput></FormInput>
+      <FormInput></FormInput>
+    </div>
+  );
+};
+
+export default Signup;
