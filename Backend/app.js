20cebd69cc887be4b896a651c635d2eea3e77f60
Elvin Posadas <elvinjosuep2@gmail.com>
2024-03-12 01:03:01 -0600

    Adding cors options

--- Backend/app.js
+++ Backend/app.js
@@ -24,7 +24,16 @@ const jwtAuthMiddleware = require("./lib/jwtAuthMiddleware");
 const app = express();
 
 // view engine setup
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://ec2-54-152-185-104.compute-1.amazonaws.com",
+      "http://localhost:5173",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
