3a0e3a806996458c6db1b6181ec7f530a281dba8
Ehylin <ehylinolezjua@gmail.com>
2024-04-07 14:17:58 +0200

    update filters search

--- FrontEnd/src/Components/ui/Filters.jsx
+++ FrontEnd/src/Components/ui/Filters.jsx
@@ -1,4 +1,5 @@
-import Input from "./Input";
+import React from 'react';
+import Input from './Input';
 
 function Filters({
   name,
@@ -9,45 +10,70 @@ function Filters({
   onChange,
   onTagsChange,
 }) {
-  const tags = ["Motor", "Electronics", "Lifestyle", "Work"];
+  const tags = ['Motor', 'Electronics', 'Lifestyle', 'Work'];
   return (
-    <div>
-      <p>Filters: </p>
-      <div className="flex flex-wrap">
-        <div className="w-full md:w-1/4 mr-4">
-          <label htmlFor="name">Name: </label>
-          <Input type="text" name="name" value={name} onChange={onChange} />
+    <div className='bg-gray-100 rounded-lg p-2'>
+      <div className='flex flex-wrap place-content-around gap-2'>
+        <div className='col-span-5 grow shrink-0'>
+          <Input
+            type='text'
+            name='name'
+            placeholder='Search'
+            value={name}
+            onChange={onChange}
+          />
         </div>
-        <div className="w-full md:w-1/4 mr-4">
-          <label htmlFor="sale">Type: </label>
-          <select name="sale" value={sale} onChange={onChange}>
-            <option></option>
-            <option>For sale</option>
-            <option>Looking to buy</option>
+        <div className=''>
+          <select
+            className='rounded-md border-0 p-2 text-gray-400 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-4 focus:ring-inset focus:ring-secondary'
+            name='sale'
+            value={sale}
+            onChange={onChange}
+          >
+            <option>All</option>
+            <option>Sale</option>
+            <option>Buy</option>
           </select>
         </div>
-        <div className="w-full md:w-1/4 mr-4">
-          <label htmlFor="max-price">Max Price: </label>
-          <Input type="number" name="maxPrice" value={maxPrice} onChange={onChange} />
+        <div className=''>
+          <Input
+            type='number'
+            name='minPrice'
+            placeholder='Min price'
+            value={minPrice}
+            onChange={onChange}
+          />
         </div>
-        <div className="w-full md:w-1/4 mr-4">
-          <label htmlFor="min-price">Min Price: </label>
-          <Input type="number" name="minPrice" value={minPrice} onChange={onChange} />
+				<div className=''>
+          <Input
+            type='number'
+            name='maxPrice'
+            placeholder='Max price'
+            value={maxPrice}
+            onChange={onChange}
+            maxLength="5"
+          />
         </div>
-      </div>
-      <div className="flex flex-wrap">
-        {tags.map((tag, index) => (
-          <div key={index} className="mr-4">
-            <input
-              type="checkbox"
-              id={tag}
-              value={tag}
-              checked={selectedTags.includes(tag)}
-              onChange={() => onTagsChange(tag)}
-            />
-            <label htmlFor={tag}>{tag}</label>
-          </div>
-        ))}
+        <div className='flex inline-block p-1 '>
+          {tags.map((tag, index) => (
+            <div key={index} className='mr-2'>
+              <label
+                htmlFor={tag}
+                className='flex cursor-pointer items-center justify-center rounded-full border border-gray-200 bg-white px-2 py-1 text-gray-400 hover:border-gray-200 has-[:checked]:border-primary has-[:checked]:primary has-[:checked]:text-gray-700'
+              >
+                <input
+                  type='checkbox'
+                  id={tag}
+                  value={tag}
+                  checked={selectedTags.includes(tag)}
+                  onChange={() => onTagsChange(tag)}
+                  className='sr-only'
+                />
+                <p className='text-sm font-medium'>{tag}</p>
+              </label>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
