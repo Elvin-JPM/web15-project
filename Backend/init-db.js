145ab8d98d0ddfcc1defde8093957f7c33c12f62
Gianluca Pettenon <gianlucapettenon@gmail.com>
2024-04-10 10:34:03 +0200

    filters styles update

--- Backend/init-db.js
+++ Backend/init-db.js
@@ -1,139 +1,68 @@
-<<<<<<< HEAD
-"use strict";
+'use strict';
 
-const readline = require("readline");
-const connection = require("./lib/connectMongoose");
-const Product = require("./models/Product");
-const initData = require("./initProducts.json");
-const User = require("./models/User");
+const readline = require('readline');
+const connection = require('./lib/connectMongoose');
+const Product = require('./models/Product');
+const initData = require('./initProducts.json');
+const User = require('./models/User');
 
-main().catch((err) => console.log("Hubo un error", err));
+main().catch(err => console.log('Hubo un error', err));
 
 async function main() {
-  await new Promise((resolve) => connection.once("open", resolve));
+	await new Promise(resolve => connection.once('open', resolve));
 
-  const borrar = await pregunta(
-    "¿Estas seguro de que quieres borrar la base de datos y cargar datos iniciales? Si/No   "
-  );
-  if (!borrar) {
-    process.exit();
-  }
+	const borrar = await pregunta(
+		'¿Estas seguro de que quieres borrar la base de datos y cargar datos iniciales? Si/No   '
+	);
+	if (!borrar) {
+		process.exit();
+	}
 
-  // inicializar la colección de usuarios
-  await initUsers();
+	// inicializar la colección de usuarios
+	await initUsers();
 
-  // inicializar la colección de productos
-  await initProducts();
+	// inicializar la colección de productos
+	await initProducts();
 
-  connection.close();
+	connection.close();
 }
 
 async function initProducts() {
-  // delete products
-  const deleted = await Product.deleteMany();
-  console.log(`Eliminados ${deleted.deletedCount} productos`);
+	// delete products
+	const deleted = await Product.deleteMany();
+	console.log(`Eliminados ${deleted.deletedCount} productos`);
 
-  // initial Products
-  const inserted = await Product.insertMany(initData.initialProducts);
-  console.log(`Creados ${inserted.length} productos.`);
+	// initial Products
+	const inserted = await Product.insertMany(initData.initialProducts);
+	console.log(`Creados ${inserted.length} productos.`);
 }
 
 function pregunta(texto) {
-  return new Promise((resolve, reject) => {
-    const ifc = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
-    ifc.question(texto, (respuesta) => {
-      ifc.close();
-      resolve(respuesta.toLowerCase() === "si");
-    });
-  });
+	return new Promise((resolve, reject) => {
+		const ifc = readline.createInterface({
+			input: process.stdin,
+			output: process.stdout,
+		});
+		ifc.question(texto, respuesta => {
+			ifc.close();
+			resolve(respuesta.toLowerCase() === 'si');
+		});
+	});
 }
 
 async function initUsers() {
-  // delete users
-  const deleted = await User.deleteMany();
-  console.log(`Eliminados ${deleted.deletedCount} usuarios`);
+	// delete users
+	const deleted = await User.deleteMany();
+	console.log(`Eliminados ${deleted.deletedCount} usuarios`);
 
-  // initial users
-  const inserted = await User.insertMany({
-    username: "user1",
-    email: "user1@example.com",
-    password: await User.hashPassword("1234"),
-    active: true,
-    activeSocketIO: false,
-    resetPasswordToken: ""
-  });
-  console.log(`Creados ${inserted.length} usuarios.`);
+	// initial users
+	const inserted = await User.insertMany({
+		username: 'user1',
+		email: 'user1@example.com',
+		password: await User.hashPassword('1234'),
+		active: true,
+		activeSocketIO: false,
+		resetPasswordToken: '',
+	});
+	console.log(`Creados ${inserted.length} usuarios.`);
 }
-=======
-"use strict";
-
-const readline = require("readline");
-const connection = require("./lib/connectMongoose");
-const Product = require("./models/Product");
-const initData = require("./initProducts.json");
-const User = require("./models/User");
-
-main().catch((err) => console.log("Hubo un error", err));
-
-async function main() {
-  await new Promise((resolve) => connection.once("open", resolve));
-
-  const borrar = await pregunta(
-    "¿Estas seguro de que quieres borrar la base de datos y cargar datos iniciales? Si/No   "
-  );
-  if (!borrar) {
-    process.exit();
-  }
-
-  // inicializar la colección de usuarios
-  await initUsers();
-
-  // inicializar la colección de productos
-  await initProducts();
-
-  connection.close();
-}
-
-async function initProducts() {
-  // delete products
-  const deleted = await Product.deleteMany();
-  console.log(`Eliminados ${deleted.deletedCount} productos`);
-
-  // initial Products
-  const inserted = await Product.insertMany(initData.initialProducts);
-  console.log(`Creados ${inserted.length} productos.`);
-}
-
-function pregunta(texto) {
-  return new Promise((resolve, reject) => {
-    const ifc = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
-    ifc.question(texto, (respuesta) => {
-      ifc.close();
-      resolve(respuesta.toLowerCase() === "si");
-    });
-  });
-}
-
-async function initUsers() {
-  // delete users
-  const deleted = await User.deleteMany();
-  console.log(`Eliminados ${deleted.deletedCount} usuarios`);
-
-  // initial users
-  const inserted = await User.insertMany({
-    username: "user1",
-    email: "user1@example.com",
-    password: await User.hashPassword("1234"),
-    active: true,
-    activeSocketIO: false,
-    resetPasswordToken: ""
-  });
-  console.log(`Creados ${inserted.length} usuarios.`);
-}
->>>>>>> 5d1f2bc9574fb5efac5b8918a417fbfbe4d99d08
--- FrontEnd/src/Components/ui/Filters.jsx
+++ FrontEnd/src/Components/ui/Filters.jsx
@@ -12,20 +12,12 @@ function Filters({
 }) {
   const tags = ["Motor", "Electronics", "Lifestyle", "Work"];
   return (
-    <div className="bg-gray-100 rounded-lg p-2 mb-10">
+    
+    <div className=" rounded-lg p-2 mb-10">
       <div className="flex flex-wrap place-content-around gap-2">
-        <div className="col-span-5 grow shrink-0">
-          <Input
-            type="text"
-            name="name"
-            placeholder="Search"
-            value={name}
-            onChange={onChange}
-          />
-        </div>
         <div className="">
           <select
-            className="rounded-md border-0 p-2 text-gray-400 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-4 focus:ring-inset focus:ring-secondary"
+            className=" bg-gray-100 rounded-l-lg border-[1px] bg-primary text-sm text-white font-bold p-2 text-wrap text-gray-400 shadow-sm ring-1 ring-inset ring-gray-300"
             name="sale"
             value={sale}
             onChange={onChange}
@@ -35,31 +27,45 @@ function Filters({
             <option value="Buy">Buy</option>
           </select>
         </div>
-        <div className="">
+        <div className="col-span-3 grow shrink-0">
           <Input
-            type="number"
-            name="minPrice"
-            placeholder="Min price"
-            value={minPrice}
+            className="ml-[-8px] block w-full rounded-r-lg border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-4 focus:ring-inset focus:ring-secondary sm:text-sm sm:leading-6"
+            type="text"
+            name="name"
+            placeholder="Search"
+            value={name}
             onChange={onChange}
           />
         </div>
-        <div className="">
-          <Input
-            type="number"
-            name="maxPrice"
-            placeholder="Max price"
-            value={maxPrice}
-            onChange={onChange}
-            maxLength="5"
-          />
+        <div className="flex block max-w-40">
+          <div className="">
+            <Input 
+              className=" appearance-none text-right w-full rounded-l-full border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-4 focus:ring-inset focus:ring-secondary sm:text-sm sm:leading-6"
+              type="number"
+              name="minPrice"
+              placeholder="Min"
+              value={minPrice}
+              onChange={onChange}
+            />
+          </div>
+          <div className="">
+            <Input
+            className="appearance-none w-full rounded-r-full border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-4 focus:ring-inset focus:ring-secondary sm:text-sm sm:leading-6"
+              type="number"
+              name="maxPrice"
+              placeholder="Max"
+              value={maxPrice}
+              onChange={onChange}
+              maxLength="5"
+            />
+          </div>
         </div>
-        <div className="flex inline-block p-1 ">
+        <div className="flex inline-block ">
           {tags.map((tag, index) => (
             <div key={index} className="mr-2">
               <label
                 htmlFor={tag}
-                className="flex cursor-pointer items-center justify-center rounded-full border border-gray-200 bg-white px-2 py-1 text-gray-400 hover:border-gray-200 has-[:checked]:border-primary has-[:checked]:primary has-[:checked]:text-gray-700"
+                className="flex cursor-pointer items-center justify-center rounded-full border border-gray-200 bg-white px-3 py-2 text-gray-400 hover:border-gray-200 has-[:checked]:border-primary has-[:checked]:primary has-[:checked]:text-gray-700"
               >
                 <input
                   type="checkbox"
